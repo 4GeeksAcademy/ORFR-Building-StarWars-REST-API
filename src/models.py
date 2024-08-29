f50682afd43370aba565a8259d80843ffd841ed6
octavio ruben flores ramirez <ruben94fr@hotmail.com>
2024-08-29 23:03:35 +0000

    +relation favuser

--- src/models.py
+++ src/models.py
@@ -7,6 +7,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    favorite = db.relationship('Favorites', uselist=False, back_populates='user')
 
     def __repr__(self):
         return '<User %r>' % self.username
@@ -57,3 +58,13 @@ class Planets(db.Model):
             "url": self.url
         }
 
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('User.id'), unique=True, nullable=False)
+    user = db.relationship('User', back_populates='favorite')
+
+
+    def serialize(self):
+        return {
+            "id": self.id
+        }
